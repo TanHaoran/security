06fc7219752af0f0c4b2d7b1ff7bc0157c12131e
JerryTan <thrforever@126.com>
2018-04-24 17:17:27 +0800

    3-11 使用Swagger自动生成文档 1. 添加pom依赖 2. 在Application中添加EnableSwagger2

--- security-demo/src/main/java/com/jerry/DemoApplication.java
+++ security-demo/src/main/java/com/jerry/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
+@EnableSwagger2
 public class DemoApplication {
 
     public static void main(String[] args) {
