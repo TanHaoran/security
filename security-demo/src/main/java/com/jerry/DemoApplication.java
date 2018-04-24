7d64a027633cb0f6074f39b9da8e545ae95c0cf7
JerryTan <thrforever@126.com>
2018-04-24 16:44:51 +0800

    2-3 Hello Spring Security 1. 搭建环境 2. 构建demo运行，打包后可独立运行

---
+++ security-demo/src/main/java/com/jerry/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.jerry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jerry
+ * Date: 2018/4/23
+ * Time: 10:41
+ * Description:
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Security!";
+    }
+}
